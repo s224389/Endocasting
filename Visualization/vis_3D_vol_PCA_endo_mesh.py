280608c0b13bdd3ef1f60b79d5af956ff5976aef
Christian Bjerregaard <124252443+s224389@users.noreply.github.com>
2025-06-11 18:02:50 +0200

    Create vis_3D_vol_PCA_endo_mesh.py

---
+++ Visualization/vis_3D_vol_PCA_endo_mesh.py
@@ -0,0 +1,30 @@
+# Creates a 3D rendering of the nifti volume (with emphasis on the bone)
+import nibabel as nib
+import numpy as np
+from skimage.filters import threshold_otsu
+import pyvista as pv
+
+path = '.nii'
+
+img = nib.load(path)
+data = img.get_fdata()
+
+thresh = threshold_otsu(data)
+opacity = [0.015 if v < thresh else 0.1 for v in np.linspace(np.min(data), np.max(data), 30)]
+
+# Use ImageData instead of UniformGrid
+grid = pv.ImageData()
+grid.dimensions = np.array(data.shape) + 1
+grid.origin = (0, 0, 0)
+grid.spacing = (1, 1, 1)
+grid.cell_data["values"] = data.flatten(order="F")
+
+# Set up the plotter
+plotter = pv.Plotter()
+plotter.add_volume(grid, cmap="gray", opacity=opacity) #clim=[0, 100]
+
+brain_mesh  = pv.read('.ply')
+plotter.add_mesh(brain_mesh,     color='red',   opacity=1)
+
+
+plotter.show()
